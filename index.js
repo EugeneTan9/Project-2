a7893cb0f3a34589e525022151e70aa56bc2468c
EugeneTan9 <tyx.eugene@gmail.com>
2019-10-30 09:22:50 +0800

    Modified

--- index.js
+++ index.js
@@ -416,19 +416,6 @@ app.get('/project/create', (request, response) => {
  * Listen to requests on port 3000
  * ===================================
  */
-const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+const PORT = process.env.PORT || 3000;
 
-let onClose = function(){
-
-  console.log("closing");
-
-  server.close(() => {
-
-    console.log('Process terminated');
-
-    pool.end( () => console.log('Shut down db connection pool'));
-  })
-};
-
-process.on('SIGTERM', onClose);
-process.on('SIGINT', onClose);
+const server = app.listen(PORT, () => console.log('~~~ Tuning in to the waves of port '+PORT+' ~~~'));
